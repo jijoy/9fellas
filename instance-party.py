9f975c9258a4828556aaa9f9fb266fc7b938916f
Jijoy <jijoy21@gmail.com>
2015-04-11 15:27:53 +0530

    Changed the implemention to use Queues

--- instance-party.py
+++ instance-party.py
@@ -1,10 +1,12 @@
 import os
 import json
-import threading
+from threading import Thread
 import time
 from time import sleep
 from flask import Flask, json, render_template, request
 import redis
+
+from Queue import Queue
 
 app = Flask(__name__)
 port = int(os.getenv("PORT"))
@@ -13,11 +15,50 @@ svc = vcap['rediscloud'][0]['credentials']
 
 db = redis.StrictRedis(host=svc["hostname"], port=svc["port"], password=svc["password"],db=0)
 
+
+
+class Producer(Thread):
+    def __init__(self,queue):
+        Thread.__init__(self)
+        self.queue = queue 
+    def run(self):
+        while True :
+            try:
+                instance_id = os.getenv("CF_INSTANCE_INDEX")
+                self.queue.put(instance_id)
+            except :
+                pass
+            finally:
+                pass
+class Consumer(Thread):
+    def __init__(self,queue):
+        Thread.__init__(self)
+        self.queue = queue
+    
+    def run(self):
+        while True :
+            try :
+                instance_id = self.queue.get()
+                db.hset("party",instance_id,time.time())
+            except:
+                pass
+            finally:
+                pass
+            
 def heartbeat():
     while True:
         instance_id = os.getenv("CF_INSTANCE_INDEX")
         db.hset("party",instance_id,time.time())
         sleep(0.5)
+        
+def init_workers():
+    party_queue = Queue()
+    p = Producer(party_queue)
+    p.daemon = True
+    c = Consumer(party_queue)
+    c.deamon= True
+    p.start()
+    c.start()
 
 @app.route('/register')
 def register():
@@ -33,6 +74,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    t = threading.Thread(target=heartbeat)
-    t.start()
+#     t = threading.Thread(target=heartbeat)
+#     t.start()
+    init_workers()
     app.run(host='0.0.0.0', port=port, debug=True)
